75c868183cae612072be6b19a89caa571ee82e87
OlexandrYaremchuk <70215770+OlexandrYaremchuk@users.noreply.github.com>
2024-03-08 14:21:52 +0200

    s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import NewComp from "./components/NewComp";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      Hello my first project
+      <NewComp />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NewComp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewComp(props) {
+  return (
+    <div style={{ backgroundColor: "red", width: "100vw", height: "100vh" }}>
+      <h1>React si very povwerfull</h1>
+    </div>
+  );
+}
+
+export default NewComp;
