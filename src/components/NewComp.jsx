26a99f9ab23981555480c3d32b3a0f583fa1d8b0
OlexandrYaremchuk <70215770+OlexandrYaremchuk@users.noreply.github.com>
2024-03-08 17:19:57 +0200

    1

--- src/components/NewComp.jsx
+++ src/components/NewComp.jsx
@@ -4,6 +4,12 @@ function NewComp(props) {
   return (
     <div style={{ backgroundColor: "red", width: "100vw", height: "100vh" }}>
       <h1>React si very povwerfull</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni,
+        repellat asperiores quis nemo debitis unde possimus? Laboriosam sed
+        expedita quia, ullam placeat repellat quos illo, cumque temporibus
+        dolorum eum asperiores.
+      </p>
     </div>
   );
 }
